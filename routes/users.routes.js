f1c812914bd32aff35816f2c4babe73923cdf982
jazzyyss <jaspreetbrar1636@gmail.com>
2023-03-03 20:00:46 -0500

    checked and redid the login functionality

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -9,6 +9,9 @@ const auth = require("../middlewares/auth")
 router.post('/register', async (req, res)=>{
     const {firstName, lastName, username, email, password} = req.body
     console.log(req.body)
+    const user = await User.findOne({username})
+    if(user) {res.json({status: 'error', message: "user already exists"}); return}
+    console.log("after  user")
     const pass = await bcrypt.hash(password, 7)
     try{
         const user = await  User.create({
@@ -21,16 +24,16 @@ router.post('/register', async (req, res)=>{
         user.save()
         res.json({status: 'OK', message: "user created successfully"})
     }catch(er){
-        res.json({status: 'error', error: er})
+        res.json({status: 'error', message: er})
     }
 })
 
 router.post('/login', async (req, res)=>{
     const {username, password} = req.body
     const user = await User.findOne({username})
-    if(!user) return {status: 'error', message: "user doesn't exist"}
+    if(!user) return res.json({status: 'error', message: "user doesn't exist"})
     const isPassValid = await bcrypt.compare(password, user.password)
-    if(!isPassValid) return {status: 'error', message: "please check your password"}
+    if(!isPassValid) return res.json({status: 'error', message: "please check your password"})
     const token = jwt.sign({username: user.username, email: user.email}, 'secretKey12')
     res.json({status: 'OK', message: token})
 })
